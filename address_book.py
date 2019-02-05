ab28a2834816355a873bb5e910c74a1a4ead67be
daesy13 <daesyramos@hotmail.com>
2019-02-05 21:51:39 +0000

    creating class with regex

--- address_book.py
+++ address_book.py
@@ -63,12 +63,38 @@ names_file.close()
 # ''', string, re.X|re.M)
 
 #COMPILING AND LOOPS
-line = re.compile(r'''
-    ^(?P<name>[-\w ]*,\s[-\w ]+)\t #last & first names
-    (?P<email>[-\w\d.+]+@[-\w\d.]+)\t #email
-    (?P<phone>\(?\d{3}\)?-?\s?\d{3}-\d{4})?\t #phone
-    (?P<job>[\w\s]+,\s[\w\s.]+)\t? #job and company
-    (?P<twitter>@[\w\d]+)?$ #twitter
-''', data, re.X|re.M)
+# line = re.compile(r'''
+#     ^(?P<name>(?P<last>[-\w ]*),\s(?P<first>[-\w ]+))\t #last & first names
+#     (?P<email>[-\w\d.+]+@[-\w\d.]+)\t #email
+#     (?P<phone>\(?\d{3}\)?-?\s?\d{3}-\d{4})?\t #phone
+#     (?P<job>[\w\s]+,\s[\w\s.]+)\t? #job and company
+#     (?P<twitter>@[\w\d]+)?$ #twitter
+# ''', re.X|re.M)
 
-print(re.search(line, data).groupdict())
+# # print(re.search(line, data).groupdict())
+# # print(line.search(data).groupdict())
+
+# for match in line.finditer(data):
+#     # print(match.group('name'))
+#     print('{first} {last} <{email}>'.format(**match.groupdict()))
+
+##### create a variable players
+# string = '''Love, Kenneth: 20
+# Chalkley, Andrew: 25
+# McFarland, Dave: 10
+# Kesten, Joy: 22
+# Stewart Pinchback, Pinckney Benton: 18'''
+# players = re.search(r'(?P<last_name>[\w ]+),\s(?P<first_name>[\w ]+):\s(?P<score>[\d]+)', string, re.M)
+
+#*****create a class named Player that has those same three attributes, last_name, first_name, and score. I should be able to set them through __init__
+
+class Player:
+
+    def __init__(self, last_name, first_name, score):
+        self.last_name = last_name
+        self.first_name = first_name
+        self.score = score
+
+
+
+
