8cb27ed77388b2660e09f996d63a2317322ee5b6
daesy13 <daesyramos@hotmail.com>
2019-02-05 19:15:43 +0000

    adding sample test for groups

--- address_book.py
+++ address_book.py
@@ -30,7 +30,40 @@ names_file.close()
 #     ''', data, re.X))
 
 #GROUPS
-line = re.search(r'''
+# line = re.search(r'''
+#     ^(?P<name>[-\w ]*,\s[-\w ]+)\t #last & first names
+#     (?P<email>[-\w\d.+]+@[-\w\d.]+)\t #email
+#     (?P<phone>\(?\d{3}\)?-?\s?\d{3}-\d{4})?\t #phone
+#     (?P<job>[\w\s]+,\s[\w\s.]+)\t? #job and company
+#     (?P<twitter>@[\w\d]+)?$ #twitter
+# ''', data, re.X|re.M)
+# print(line)
+# print(line.groupdict())
+
+# sample
+# string = 'Perotto, Pier Giorgio'
+# names = re.match(r'''
+#     ^(?P<firstname>[\w]*),\s #first name
+#     (?P<lastname>[\w\s\w]*)$ #last name
+# ''', string, re.X)
+
+# string = '''Love, Kenneth, kenneth+challenge@teamtreehouse.com, 555-555-5555, @kennethlove
+# Chalkley, Andrew, andrew@teamtreehouse.co.uk, 555-555-5556, @chalkers
+# McFarland, Dave, dave.mcfarland@teamtreehouse.com, 555-555-5557, @davemcfarland
+# Kesten, Joy, joy@teamtreehouse.com, 555-555-5558, @joykesten'''
+
+# contacts = re.search(r'''
+#     (?P<email>[-\w\d.+]+@[-\w\d.]+),\s #email
+#     (?P<phone>\d{3}-\d{3}-\d{4}) #phone
+# ''', string, re.X|re.M)
+
+# create a twitters variable
+# twitters = re.search(r'''
+#     (?P<twitter>@[\w\d]+)$ #twitter
+# ''', string, re.X|re.M)
+
+#COMPILING AND LOOPS
+line = re.compile(r'''
     ^(?P<name>[-\w ]*,\s[-\w ]+)\t #last & first names
     (?P<email>[-\w\d.+]+@[-\w\d.]+)\t #email
     (?P<phone>\(?\d{3}\)?-?\s?\d{3}-\d{4})?\t #phone
@@ -38,21 +71,4 @@ line = re.search(r'''
     (?P<twitter>@[\w\d]+)?$ #twitter
 ''', data, re.X|re.M)
 
-# print(line)
-print(line.groupdict())
-
-# line = re.compile(r'''
-#     ^(?P<name>(?P<last>[-\w ]*),\s(?P<first>[-\w ]+))\t  # Last and first names
-#     (?P<email>[-\w\d.+]+@[-\w\d.]+)\t  # Email
-#     (?P<phone>\(?\d{3}\)?-?\s?\d{3}-\d{4})?\t  # Phone
-#     (?P<job>[\w\s]+,\s[\w\s.]+)\t?  # Job and company
-#     (?P<twitter>@[\w\d]+)?$  # Twitter
-# ''', re.X|re.M)
-
-
-
-
-
-
-
-
+print(re.search(line, data).groupdict())
